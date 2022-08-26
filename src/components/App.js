fd5345c9e4aeb5dc11d4b133ad57c4b6fcc62fcb
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-08-26 20:37:24 +0200

    Add html and scss header

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,16 @@ import {Link, Route, Routes} from 'react-router-dom';
 //import PropTypes from 'prop-types';
 
 
-//IMAGES//
-import logoHeader from '../images/header-hp.png';
-import logoFooter from '../images/deathly-hallows.png';
-
 
 //SERVICES//
 import getDataApi from '../services/charactersHarryPotterApi';
 
+//COMPONENTS//
+import Header from "./Header";
 
 function App() {
 
-  //VARIABLES DE ESTADO//
+  //STATE VARIABLES//
 
   const [dataCharater, setDataCharater] = useState([]);
 
@@ -30,16 +28,13 @@ function App() {
     }, []);
   
   return (
-    <div>
-      <header>
-        <image src={logoHeader} alt="Harry Potter"></image>
-      </header>
+    <div className="completeHtml">
+      <Header />
       <main>
         <section></section>
 
       </main>
       <footer>
-        <image src={logoFooter} alt="Logo las reliquias de la muerte"></image>
         <div>
           <p>Created by Yaiza Soria Felices 2022</p>
           
