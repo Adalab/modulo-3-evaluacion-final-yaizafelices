586fbd64fc4c6e79fc7baec2019f0ecb65c9fe2b
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-08-26 18:08:57 +0200

    Add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// import React from "react";
+import '../styles/App.scss';
+
+
+//import PropTypes from 'prop-types';
+
+// import {Link,Route} from 'react-router-dom';
+
+// import { useState } from "react";
+function App() {
+  return (
+    <div> <h1>Hola mundo</h1> </div>
+  );
+}
+export default App;
+
