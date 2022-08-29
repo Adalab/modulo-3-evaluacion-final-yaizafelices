b6adbea8a57b63f18dca2d1c5def392e7c0cedfe
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-08-29 19:28:40 +0200

    Add css to the character detail

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -10,13 +10,15 @@ const CharacterCard =(props)=> {
     return (
         <li className="character">
             <Link to={`/character/${props.character.id}`}>
-                <img
-                    src={props.character.image}
-                    className="character__image"
-                    alt={`Foto de ${props.character.name}`}
-                    title={`Foto de ${props.character.name}`}></img>
-                <h4 className="character__data">{props.character.name}</h4>
-                <p className="character__data">{`${props.character.species}`}</p>               
+                <article>
+                    <img
+                        src={props.character.image}
+                        className="character__image"
+                        alt={`Foto de ${props.character.name}`}
+                        title={`Foto de ${props.character.name}`}></img>
+                    <h4 className="character__data">{props.character.name}</h4>
+                    <p className="character__data">{`${props.character.species}`}</p>
+                </article>               
             </Link>
         </li>)
 
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,25 +1,30 @@
 import { Link } from "react-router-dom";
+import '../styles/CharacterDetail.scss';
 
 const CharacterDetail=(props) =>{
 
     return (
-        <section className="character__detail">
-            <Link to='/' className=''>
-                <p className='character__detail-link'>Volver</p>
-                <i class="fa-solid fa-wand-sparkles"></i>
+        <section className="detail">
+            <Link to='/' className='detail__link'>
+                    <p >Volver</p>
+                    <i class="fa-solid fa-wand-sparkles"></i>
             </Link>
-                <img
-                    src={props.character.image}
-                    className="character__detail-image"
-                    alt={`Foto de ${props.character.name}`}
-                    title={`Foto de ${props.character.name}`}>
-                </img>
-                <h4 className="character__detail-data">{props.character.name}</h4>
-                <p className="character__detail-data">{`${props.character.house}`}</p>
-                <p className="character__detail-data">{`${props.character.alive}`}</p>
-                <p className="character__detail-data">{`${props.character.gender}`}</p>
-                <p className="character__detail-data">{`${props.character.species}`}</p>
-                <p className="character__detail-data">{`${props.character.alternate_names}`}</p>
+                <article className="detail__card">
+                    <img
+                        src={props.character.image}
+                        className="detail__card-image"
+                        alt={`Foto de ${props.character.name}`}
+                        title={`Foto de ${props.character.name}`}>
+                    </img>
+                    <div className="detail__card-data">
+                        <h4>{props.character.name}</h4>
+                        <p>{`Casa: ${props.character.house}`}</p>
+                        <p>{`Estatus: ${props.character.alive}`}</p>
+                        <p>{`Especie: ${props.character.gender}`}</p>
+                        <p>{`Género: ${props.character.species}`}</p>
+                        <p>{`${props.character.alternate_names}`}</p>
+                    </div>
+                </article>
         </section>)
 }
 export default CharacterDetail;
